989c17748dfa450f617939f56edb21f24decb9d7
mandinga25 <gusta.king.25@gmail.com>
2019-11-19 20:41:44 -0300

    Cambio de colores favoritos

--- src/colorescristian/ColoresCristian.java
+++ src/colorescristian/ColoresCristian.java
@@ -4,9 +4,9 @@ public class ColoresCristian {
 
     public static void main(String[] args) {
         String color1, color2, color3;
-        color1 = "AZUL";
+        color1 = "NEGRO";
         color2 = "ROJO";
-        color3 = "VERDE";
+        color3 = "BLANCO";
         System.out.printf("%s es mi primer color favorito\n", color1);
         System.out.printf("%s es mi segundo color favorito\n", color2);
         System.out.printf("%s es mi tercer color favorito\n", color3);
